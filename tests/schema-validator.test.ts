1fbd4509dfd1091fe62e1cd1988a7fbe1a069c17
Nathan Brown <nathan.brown@bjss.com>
2023-06-13 15:36:26 +0100

    feat(cb2-8457): add new test to confirm it's stripping values

--- tests/schema-validator.test.ts
+++ tests/schema-validator.test.ts
@@ -5,4 +5,11 @@ describe('test schema validation', () => {
         const res = isValidObject('v3/tech-record/get/psv/skeleton', {})
         expect(res).toBe(false)
     })
+
+    it('should remove pieces of data that are not being validated', () => {
+        const data = { foo: "bar" }
+        const res = isValidObject('v3/tech-record/get/psv/skeleton', data)
+        expect(data).toEqual({})
+        expect(res).toBe(false)
+    })  
 })
