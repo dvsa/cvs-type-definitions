6d21f646c3e26d0941ec78a95f0aa9f3e21ecaf5
Shivangi Das <shivangi.das7@gmail.com>
2023-07-28 16:53:42 +0100

    test(*): add tests

--- tests/schema-validator.test.ts
+++ tests/schema-validator.test.ts
@@ -15,4 +15,24 @@ describe("test schema validation", () => {
     expect(data).toEqual({});
     expect(res).toBe(false);
   });
+
+  it("should return errors when returnErrors is true", () => {
+    const result1 = isValidObject("v3/tech-record/put/car/skeleton/index.json", {vin: "testVIN", techRecord_vehicleType: "123"}, false, true);
+    
+    expect(result1).toHaveLength(1);
+    expect(result1).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+      instancePath: "/techRecord_vehicleType",
+      message: "must be equal to one of the allowed values",
+      params: {"allowedValues": ["psv", "trl", "hgv", "car", "lgv", "motorcycle"]},
+      })]
+    ));
+
+    const result2 = isValidObject("v3/tech-record/put/car/skeleton/index.json", {}, false, true);
+    expect(result2).toHaveLength(2);
+    expect(result2).toEqual(expect.arrayContaining([
+      expect.objectContaining({instancePath: "", message: "must have required property 'vin'"}),
+      expect.objectContaining({instancePath: "", message: "must have required property 'techRecord_vehicleType'"})
+    ]))
+  })
 });
