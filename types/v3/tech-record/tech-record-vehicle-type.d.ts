8e11a10e2ed9e53cee5c21327d8b67bd699a42bf
Guillaume Julien (BJSS) <104442665+gjulien-bjss@users.noreply.github.com>
2023-08-18 10:36:26 +0100

    patch(*): use relative paths for manual types (#65)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- types/v3/tech-record/tech-record-vehicle-type.d.ts
+++ types/v3/tech-record/tech-record-vehicle-type.d.ts
@@ -1,35 +1,32 @@
-import {
-  TechRecordGETCarComplete,
-  VehicleType,
-} from "types/v3/tech-record/get/car/complete";
-import { TechRecordGETCarSkeleton } from "types/v3/tech-record/get/car/skeleton";
-import { TechRecordGETHGVComplete } from "types/v3/tech-record/get/hgv/complete";
-import { TechRecordGETHGVSkeleton } from "types/v3/tech-record/get/hgv/skeleton";
-import { TechRecordGETHGVTestable } from "types/v3/tech-record/get/hgv/testable";
-import { TechRecordGETLGVComplete } from "types/v3/tech-record/get/lgv/complete";
-import { TechRecordGETLGVSkeleton } from "types/v3/tech-record/get/lgv/skeleton";
-import { TechRecordGETMotorcycleComplete } from "types/v3/tech-record/get/motorcycle/complete";
-import { TechRecordGETMotorcycleSkeleton } from "types/v3/tech-record/get/motorcycle/skeleton";
-import { TechRecordGETPSVComplete } from "types/v3/tech-record/get/psv/complete";
-import { TechRecordGETPSVSkeleton } from "types/v3/tech-record/get/psv/skeleton";
-import { TechRecordGETPSVTestable } from "types/v3/tech-record/get/psv/testable";
-import { TechRecordGETTRLComplete } from "types/v3/tech-record/get/trl/complete";
-import { TechRecordGETTRLSkeleton } from "types/v3/tech-record/get/trl/skeleton";
-import { TechRecordGETTRLTestable } from "types/v3/tech-record/get/trl/testable";
-import { TechRecordPUTCarComplete } from "types/v3/tech-record/put/car/complete";
-import { TechRecordPUTCarSkeleton } from "types/v3/tech-record/put/car/skeleton";
-import { TechRecordPUTHGVComplete } from "types/v3/tech-record/put/hgv/complete";
-import { TechRecordPUTHGVSkeleton } from "types/v3/tech-record/put/hgv/skeleton";
-import { TechRecordPUTHGVTestable } from "types/v3/tech-record/put/hgv/testable";
-import { TechRecordPUTLGVComplete } from "types/v3/tech-record/put/lgv/complete";
-import { TechRecordPUTLGVSkeleton } from "types/v3/tech-record/put/lgv/skeleton";
-import { TechRecordPUTMotorcycleComplete } from "types/v3/tech-record/put/motorcycle/complete";
-import { TechRecordPUTPSVComplete } from "types/v3/tech-record/put/psv/complete";
-import { TechRecordPUTPSVSkeleton } from "types/v3/tech-record/put/psv/skeleton";
-import { TechRecordPUTPSVTestable } from "types/v3/tech-record/put/psv/testable";
-import { TechRecordPUTTRLComplete } from "types/v3/tech-record/put/trl/complete";
-import { TechRecordPUTTRLSkeleton } from "types/v3/tech-record/put/trl/skeleton";
-import { TechRecordPUTTRLTestable } from "types/v3/tech-record/put/trl/testable";
+import { TechRecordGETCarComplete } from "./get/car/complete";
+import { TechRecordGETCarSkeleton } from "./get/car/skeleton";
+import { TechRecordGETHGVComplete } from "./get/hgv/complete";
+import { TechRecordGETHGVSkeleton } from "./get/hgv/skeleton";
+import { TechRecordGETHGVTestable } from "./get/hgv/testable";
+import { TechRecordGETLGVComplete, VehicleType } from "./get/lgv/complete";
+import { TechRecordGETLGVSkeleton } from "./get/lgv/skeleton";
+import { TechRecordGETMotorcycleComplete } from "./get/motorcycle/complete";
+import { TechRecordGETMotorcycleSkeleton } from "./get/motorcycle/skeleton";
+import { TechRecordGETPSVComplete } from "./get/psv/complete";
+import { TechRecordGETPSVSkeleton } from "./get/psv/skeleton";
+import { TechRecordGETPSVTestable } from "./get/psv/testable";
+import { TechRecordGETTRLComplete } from "./get/trl/complete";
+import { TechRecordGETTRLSkeleton } from "./get/trl/skeleton";
+import { TechRecordGETTRLTestable } from "./get/trl/testable";
+import { TechRecordPUTCarComplete } from "./put/car/complete";
+import { TechRecordPUTCarSkeleton } from "./put/car/skeleton";
+import { TechRecordPUTHGVComplete } from "./put/hgv/complete";
+import { TechRecordPUTHGVSkeleton } from "./put/hgv/skeleton";
+import { TechRecordPUTHGVTestable } from "./put/hgv/testable";
+import { TechRecordPUTLGVComplete } from "./put/lgv/complete";
+import { TechRecordPUTLGVSkeleton } from "./put/lgv/skeleton";
+import { TechRecordPUTMotorcycleComplete } from "./put/motorcycle/complete";
+import { TechRecordPUTPSVComplete } from "./put/psv/complete";
+import { TechRecordPUTPSVSkeleton } from "./put/psv/skeleton";
+import { TechRecordPUTPSVTestable } from "./put/psv/testable";
+import { TechRecordPUTTRLComplete } from "./put/trl/complete";
+import { TechRecordPUTTRLSkeleton } from "./put/trl/skeleton";
+import { TechRecordPUTTRLTestable } from "./put/trl/testable";
 
 export type TechRecordCar =
   | TechRecordGETCarComplete
