fa65ab0f9d0c0852638d17a3281dc7c9b5823632
Leo <leo.ginever@bjss.com>
2023-10-09 13:37:48 +0100

    fix(cb-9249): updated lgv complete schema to include vehicle config

--- types/v3/tech-record/put/lgv/complete/index.d.ts
+++ types/v3/tech-record/put/lgv/complete/index.d.ts
@@ -26,6 +26,19 @@ export type EUVehicleCategory =
   | "l7e";
 export type StatusCode = "provisional" | "current" | "archived";
 export type VehicleSubclass = ("n" | "p" | "a" | "s" | "c" | "l" | "t" | "e" | "m" | "r" | "w")[];
+export type VehicleConfiguration =
+  | "rigid"
+  | "articulated"
+  | "centre axle drawbar"
+  | "semi-car transporter"
+  | "semi-trailer"
+  | "long semi-trailer"
+  | "low loader"
+  | "other"
+  | "drawbar"
+  | "four-in-line"
+  | "dolly"
+  | "full drawbar";
 
 export interface TechRecordPUTLGVComplete {
   vin: string;
@@ -50,4 +63,5 @@ export interface TechRecordPUTLGVComplete {
   techRecord_hiddenInVta?: boolean;
   techRecord_updateType?: string;
   secondaryVrms?: string[];
+  techRecord_vehicleConfiguration?: null | VehicleConfiguration;
 }
