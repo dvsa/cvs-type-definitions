2ff521397e6f4d5c8dc0650bc0f10dc0174f4e3b
owen <owen.corrigan@bjss.com>
2023-11-28 16:45:16 +0000

    feat(cb2-9864): changes tc3 to array of details

--- types/v3/tech-record/get/lgv/skeleton/index.d.ts
+++ types/v3/tech-record/get/lgv/skeleton/index.d.ts
@@ -52,9 +52,7 @@ export interface TechRecordGETLGVSkeleton {
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2Type?: null | TC2Types;
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2IntermediateApprovalNo?: string | null;
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2IntermediateExpiryDate?: string | null;
-  techRecord_adrDetails_tank_tankDetails_tc3Details_tc3Type?: null | TC3Types;
-  techRecord_adrDetails_tank_tankDetails_tc3Type_tc3PeriodicNumber?: string | null;
-  techRecord_adrDetails_tank_tankDetails_tc3Type_tc3PeriodicExpiryDate?: string | null;
+  techRecord_adrDetails_tank_tankDetails_tc3Details?: TC3Details[] | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_substancesPermitted?: string | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_statement?: string | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_productListRefNo?: string | null;
@@ -85,6 +83,11 @@ export interface TechRecordGETLGVSkeleton {
   techRecord_updateType?: null | string;
   secondaryVrms?: null | string[];
   techRecord_vehicleSubclass?: VehicleSubclass;
+}
+export interface TC3Details {
+  tc3Type?: null | TC3Types;
+  tc3Type_tc3PeriodicNumber?: string | null;
+  tc3Type_tc3PeriodicExpiryDate?: string | null;
 }
 
 export enum ADRCompatibilityGroupJ {
--- types/v3/tech-record/put/trl/testable/index.d.ts
+++ types/v3/tech-record/put/trl/testable/index.d.ts
@@ -141,9 +141,7 @@ export interface TechRecordPUTTRLTestable {
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2Type?: null | TC2Types;
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2IntermediateApprovalNo?: string | null;
   techRecord_adrDetails_tank_tankDetails_tc2Details_tc2IntermediateExpiryDate?: string | null;
-  techRecord_adrDetails_tank_tankDetails_tc3Details_tc3Type?: null | TC3Types;
-  techRecord_adrDetails_tank_tankDetails_tc3Type_tc3PeriodicNumber?: string | null;
-  techRecord_adrDetails_tank_tankDetails_tc3Type_tc3PeriodicExpiryDate?: string | null;
+  techRecord_adrDetails_tank_tankDetails_tc3Details?: TC3Details[] | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_substancesPermitted?: string | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_statement?: string | null;
   techRecord_adrDetails_tank_tankDetails_tankStatement_productListRefNo?: string | null;
@@ -240,6 +238,11 @@ export interface TechRecordPUTTRLTestable {
   techRecord_authIntoService_datePending?: string | null;
   techRecord_authIntoService_dateAuthorised?: string | null;
   techRecord_authIntoService_dateRejected?: string | null;
+}
+export interface TC3Details {
+  tc3Type?: null | TC3Types;
+  tc3Type_tc3PeriodicNumber?: string | null;
+  tc3Type_tc3PeriodicExpiryDate?: string | null;
 }
 export interface TRLPlates {
   plateSerialNumber?: string | null;
