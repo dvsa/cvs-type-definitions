3449a2b5e97fbbf4c1af162bb2aa4eadcf2f8864
Shivangi Das <shivangi.das7@gmail.com>
2023-12-11 11:31:18 +0000

    minor(cb2-10120): add conditional logic, remove type warning

--- tests/hgv/skeleton.test.ts
+++ tests/hgv/skeleton.test.ts
@@ -31,3 +31,74 @@ describe("validate skeleton hgv schema", () => {
     expect(res).toEqual(false);
   });
 });
+describe("validate ADR hgv schema", () => {
+  it("should fail if adr fields are missing when adr is Yes", () => {
+    const data = hgvData[5];
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(false);
+  });
+  it("should pass if adr fields are missing when adr is No", () => {
+    const data = hgvData[5];
+    (data as any).techRecord_adrDetails_dangerousGoods = false;
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+  it("should pass if adr fields are present when adr is Yes", () => {
+    const data = hgvData[6];
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+  it("should fail if compatibilityGroupJ is missing when permitted goods is Explosives type 2 or 3", () => {
+    const data = hgvData[6];
+    data.techRecord_adrDetails_permittedDangerousGoods = ["Explosives (type 2)"];
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(false);
+  });
+  it("should pass if compatibilityGroupJ is present when permitted goods is Explosives type 2 or 3", () => {
+    const data = hgvData[6];
+    data.techRecord_adrDetails_permittedDangerousGoods = ["Explosives (type 3)"];
+    (data as any).techRecord_adrDetails_compatibilityGroupJ = "I";
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+  it("should fail if techRecord_adrDetails_vehicleDetails_type is tank or battery but tank details are missing", () => {
+    const data = hgvData[6];
+    data.techRecord_adrDetails_vehicleDetails_type = "Semi trailer battery";
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(false);
+  });
+  it("should pass if techRecord_adrDetails_vehicleDetails_type is tank or battery and tank details are present", () => {
+    const data = hgvData[7];
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+  it("should fail if techRecord_adrDetails_listStatementApplicable is true but reference number is missing", () => {
+    const data = hgvData[7];
+    data.techRecord_adrDetails_vehicleDetails_type = "Rigid battery";
+    (data as any).techRecord_adrDetails_listStatementApplicable = true;
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(false);
+  });
+  it("should pass if techRecord_adrDetails_listStatementApplicable is true and reference number is present", () => {
+    const data = hgvData[7];
+    data.techRecord_adrDetails_vehicleDetails_type = "Rigid battery";
+    (data as any).techRecord_adrDetails_listStatementApplicable = true;
+    (data as any).techRecord_adrDetails_batteryListNumber = "12345678";
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+
+  it("should fail if techRecord_adrDetails_brakeEndurance is true but weight is missing", () => {
+    const data = hgvData[7];
+    (data as any).techRecord_adrDetails_brakeEndurance = true;
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(false);
+  });
+  it("should pass if techRecord_adrDetails_brakeEndurance is true and weight is present", () => {
+    const data = hgvData[7];
+    (data as any).techRecord_adrDetails_brakeEndurance = true;
+    (data as any).techRecord_adrDetails_weight = "123";
+    const res = isValidObject(schemaName, data);
+    expect(res).toEqual(true);
+  });
+});
