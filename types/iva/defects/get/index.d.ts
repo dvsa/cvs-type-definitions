243e36dbc47d08f12c96bd2aa707b82440019f43
Michael <michael.mullen@dvsa.gov.uk>
2023-12-20 14:44:22 +0000

    feat(cb2-10282): update iva type definition for new data structure

--- types/iva/defects/get/index.d.ts
+++ types/iva/defects/get/index.d.ts
@@ -7,39 +7,17 @@
 
 export type InspectionType = "basic" | "normal";
 
-export interface DefectGETIVA {
+export interface SectionIVA {
   sectionNumber: string;
   sectionDescription: string;
-  euVehicleCategories: EUVehicleCategory[];
   requiredStandards: {
     rsNumber: number;
     requiredStandard: string;
     refCalculation: string;
     additionalInfo: boolean;
-    inspectionTypes?: InspectionType[];
+    inspectionTypes: InspectionType[];
   }[];
   additionalInformation?: {
     notes: string;
   };
-}
-
-export enum EUVehicleCategory {
-  M1 = "m1",
-  M2 = "m2",
-  M3 = "m3",
-  N1 = "n1",
-  N2 = "n2",
-  N3 = "n3",
-  O1 = "o1",
-  O2 = "o2",
-  O3 = "o3",
-  O4 = "o4",
-  L1E_A = "l1e-a",
-  l1E = "l1e",
-  L2e = "l2e",
-  L3E = "l3e",
-  L4E = "l4e",
-  L5E = "l5e",
-  L6E = "l6e",
-  L7E = "l7e"
 }
