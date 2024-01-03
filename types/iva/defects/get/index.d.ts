4cf72775bfee28bce029b88e971137df20b4b4c6
Michael <michael.mullen@dvsa.gov.uk>
2024-01-03 09:23:19 +0000

    feat(cb2-10282): update euVehicleCategory : string to euVehicleCategories : EUVehicleCategory[]

--- types/iva/defects/get/index.d.ts
+++ types/iva/defects/get/index.d.ts
@@ -8,7 +8,7 @@
 export type InspectionType = "basic" | "normal";
 
 export interface DefectGETIVA {
-  euVehicleCategory: string;
+  euVehicleCategories: EUVehicleCategory[];
   basic: SectionIVA[];
   normal: SectionIVA[];
 }
@@ -23,4 +23,25 @@ export interface RequiredStandard {
   refCalculation: string;
   additionalInfo: boolean;
   inspectionTypes: InspectionType[];
+}
+
+export enum EUVehicleCategory {
+  M1 = "m1",
+  M2 = "m2",
+  M3 = "m3",
+  N1 = "n1",
+  N2 = "n2",
+  N3 = "n3",
+  O1 = "o1",
+  O2 = "o2",
+  O3 = "o3",
+  O4 = "o4",
+  L1E_A = "l1e-a",
+  l1E = "l1e",
+  L2e = "l2e",
+  L3E = "l3e",
+  L4E = "l4e",
+  L5E = "l5e",
+  L6E = "l6e",
+  L7E = "l7e"
 }
