89e4a865c307cda66a1d12f080c673d2f00a19c1
Leo <145344770+LGin-BJSS@users.noreply.github.com>
2024-08-06 10:38:46 +0100

    minor(cb2-13155): Added central docs to test type (#179)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- types/v1/test/index.d.ts
+++ types/v1/test/index.d.ts
@@ -422,7 +422,8 @@ export enum TestStationTypes {
   ATF = "atf",
   GVTS = "gvts",
   HQ = "hq",
-  POTF = "potf"
+  POTF = "potf",
+  VEF = "vef"
 }
 export enum TestStatus {
   SUBMITTED = "submitted",
