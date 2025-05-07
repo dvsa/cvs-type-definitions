cade73f25935bd180e5447aad5c08f2d67f51e66
NathanielRichards1 <nathaniel.richards@dvsa.gov.uk>
2025-05-07 11:24:09 +0100

    feat(cb2-16761): new union enum

--- types/v3/tech-record/enums/vehicleConfiguration.enum.ts
+++ types/v3/tech-record/enums/vehicleConfiguration.enum.ts
@@ -5,11 +5,32 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export enum VehicleConfiguration {
-  ARTICULATED = "articulated",
+export type VehicleConfiguration =
+  | VehicleConfigurationHgvPsv
+  | VehicleConfigurationLightVehicle
+  | VehicleConfigurationTrl;
+
+export enum VehicleConfigurationHgvPsv {
   RIGID = "rigid",
+  ARTICULATED = "articulated"
+}
+export enum VehicleConfigurationLightVehicle {
+  RIGID = "rigid",
+  ARTICULATED = "articulated",
   CENTRE_AXLE_DRAWBAR = "centre axle drawbar",
-  SEMI_CART_TRANSPORTER = "semi-car transporter",
+  SEMI_CAR_TRANSPORTER = "semi-car transporter",
+  SEMI_TRAILER = "semi-trailer",
+  LONG_SEMI_TRAILER = "long semi-trailer",
+  LOW_LOADER = "low loader",
+  OTHER = "other",
+  DRAWBAR = "drawbar",
+  FOUR_IN_LINE = "four-in-line",
+  DOLLY = "dolly",
+  FULL_DRAWBAR = "full drawbar"
+}
+export enum VehicleConfigurationTrl {
+  CENTRE_AXLE_DRAWBAR = "centre axle drawbar",
+  SEMI_CAR_TRANSPORTER = "semi-car transporter",
   SEMI_TRAILER = "semi-trailer",
   LONG_SEMI_TRAILER = "long semi-trailer",
   LOW_LOADER = "low loader",
