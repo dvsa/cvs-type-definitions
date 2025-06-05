cb5454166b2b33e1beff63aee4dd28c601b1481d
Nathaniel James Priday Richards <99956767+NathanielRichards1@users.noreply.github.com>
2025-06-05 09:12:53 +0100

    minor(cb2-17641): set visit closure reason in auto-close (#203)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- types/v1/activity/index.d.ts
+++ types/v1/activity/index.d.ts
@@ -21,6 +21,7 @@ export interface ActivitySchema {
   waitReason?: WaitReason[];
   notes?: string | null;
   activityDay?: string;
+  closureReason?: ClosureReasonType;
 }
 
 export enum ActivityType {
@@ -41,4 +42,8 @@ export enum WaitReason {
   ADMIN = "Admin",
   SITE_ISSUE = "Site issue",
   OTHER = "Other"
+}
+export enum ClosureReasonType {
+  AUTOCLOSE = "Autoclose",
+  MANUAL = "Manual"
 }
